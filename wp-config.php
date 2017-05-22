f66d602a24cbbe79294b18fb116ad66adbf7a046
James Davis <james.norman.davis@gmail.com>
2017-05-22 16:25:11 +1000

    Add AWS access keys variables

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,8 @@ define('AUTH_SALT',        $_SERVER['WP_AUTH_SALT']);
 define('SECURE_AUTH_SALT', $_SERVER['WP_SECURE_AUTH_SALT']);
 define('LOGGED_IN_SALT',   $_SERVER['WP_LOGGED_IN_SALT']);
 define('NONCE_SALT',       $_SERVER['WP_NONCE_SALT']);
+define('DBI_AWS_ACCESS_KEY_ID', $_SERVER['WP_AWS_ACCESS_KEY_ID']);
+define('DBI_AWS_SECRET_ACCESS_KEY', $_SERVER['WP_AWS_SECRET_ACCESS_KEY']);
 $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
